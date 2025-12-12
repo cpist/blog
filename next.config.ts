ddc59134b5fb990cab0b72e413682100291e3ac3
cpist <tinyfrog12@gmail.com>
2025-12-12 22:10:03 +0900

    Deploy to Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "blog"; // ← GitHub 레포 이름으로 바꾸기
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+  trailingSlash: true, // GH Pages에서 폴더 라우팅 안정적
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: { unoptimized: true }, // next/image 최적화 서버가 없으므로
 };
 
 export default nextConfig;
